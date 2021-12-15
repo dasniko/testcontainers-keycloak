436957842fb2c739f161bb20712c28645f1d2012
Niko Köbler <niko@n-k.de>
2021-12-15 09:19:16 +0100

    update self-signed tls certs
    to be able to work with CN=localhost and IP=127.0.0.1
    this might become an issue, when running with something like TestContainers.Cloud, where Dockerhost is rather "127.0.0.1" than "localhost"

--- tools/create_certs.sh
+++ tools/create_certs.sh
@@ -1,3 +1,3 @@
-openssl req -new -x509 -sha256 -newkey rsa:2048 -keyout tls.key -out tls.crt -days 3650 -nodes -subj '/CN=localhost'
+openssl req -new -x509 -sha256 -newkey rsa:2048 -keyout tls.key -out tls.crt -days 3650 -nodes -subj '/CN=localhost' -config req.cnf
 openssl pkcs12 -export -in tls.crt -inkey tls.key -out tls.p12
 keytool -importkeystore -srckeystore tls.p12 -srcstoretype PKCS12 -destkeystore tls.jks -deststoretype JKS
