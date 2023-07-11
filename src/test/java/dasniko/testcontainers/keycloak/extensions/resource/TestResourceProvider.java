961c04edb826eee4fb98f76ad9793c176b51ae1a
Niko Köbler <niko@n-k.de>
2023-07-11 17:37:25 +0200

    upgrade to KC 22.0
    - update dependencies
    - refactor test resources to use jakarta.ws.* namespace instead of javax.ws.*

--- src/test/java/dasniko/testcontainers/keycloak/extensions/resource/TestResourceProvider.java
+++ src/test/java/dasniko/testcontainers/keycloak/extensions/resource/TestResourceProvider.java
@@ -1,17 +1,17 @@
 package dasniko.testcontainers.keycloak.extensions.resource;
 
+import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotAuthorizedException;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager.AuthResult;
 import org.keycloak.services.resource.RealmResourceProvider;
 
-import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.Collections;
 
 /**
--- src/test/java/dasniko/testcontainers/keycloak/extensions/resource/YodaResourceProvider.java
+++ src/test/java/dasniko/testcontainers/keycloak/extensions/resource/YodaResourceProvider.java
@@ -1,13 +1,13 @@
 package dasniko.testcontainers.keycloak.extensions.resource;
 
 import com.github.javafaker.Faker;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.keycloak.services.resource.RealmResourceProvider;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.Collections;
 
 /**
