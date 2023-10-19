2312d82c0d5c8ee78ae6e43e2195389cc6bc829a
Niko Köbler <niko@n-k.de>
2023-10-19 21:41:33 +0200

    update to the latest patch version, as they have introduced some incompatible change in the api...

--- src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
+++ src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
@@ -57,7 +57,7 @@ public abstract class ExtendableKeycloakContainer<SELF extends ExtendableKeycloa
     public static final String ADMIN_CLI_CLIENT = "admin-cli";
 
     private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak";
-    private static final String KEYCLOAK_VERSION = "22.0";
+    private static final String KEYCLOAK_VERSION = "22.0.4";
 
     private static final int KEYCLOAK_PORT_HTTP = 8080;
     private static final int KEYCLOAK_PORT_HTTPS = 8443;
