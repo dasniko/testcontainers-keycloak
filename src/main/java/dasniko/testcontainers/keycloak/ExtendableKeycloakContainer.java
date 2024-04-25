56036dfde79cf1615b398b168fcb0bc66d4a03da
Niko Köbler <niko@n-k.de>
2024-04-25 13:28:25 +0200

    copy realm file with permissions
    resolves #137

--- src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
+++ src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
@@ -190,7 +190,7 @@ public abstract class ExtendableKeycloakContainer<SELF extends ExtendableKeycloa
             for (String importFile : importFiles) {
                 // TODO: a strategy for files with the same name but in the different dirs
                 String importFileInContainer = DEFAULT_REALM_IMPORT_FILES_LOCATION + FilenameUtils.getName(importFile);
-                withCopyFileToContainer(MountableFile.forClasspathResource(importFile), importFileInContainer);
+                withCopyFileToContainer(MountableFile.forClasspathResource(importFile, 0777), importFileInContainer);
             }
             commandParts.add("--import-realm");
         }
