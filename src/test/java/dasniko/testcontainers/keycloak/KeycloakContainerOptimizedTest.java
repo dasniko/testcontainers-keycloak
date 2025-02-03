56a4da41ab445321b28218dde016921ff14641b1
Robin Meese <39960884+robson90@users.noreply.github.com>
2025-02-03 17:59:39 +0100

    Remove temporary image after test run
    Signed-off-by: Robin Meese <39960884+robson90@users.noreply.github.com>

--- src/test/java/dasniko/testcontainers/keycloak/KeycloakContainerOptimizedTest.java
+++ src/test/java/dasniko/testcontainers/keycloak/KeycloakContainerOptimizedTest.java
@@ -1,8 +1,11 @@
 package dasniko.testcontainers.keycloak;
 
+import com.github.dockerjava.api.exception.NotFoundException;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
@@ -17,15 +20,16 @@ import static org.hamcrest.Matchers.startsWith;
 public class KeycloakContainerOptimizedTest {
 
     public static final String UPDATING_THE_CONFIGURATION = "Updating the configuration and installing your custom providers, if any. Please wait.";
+    private static final String TEMPORARY_KEYCLOAK_IMAGE = "temporary-testcontainer-keycloak-image:latest";
     private KeycloakContainer keycloakContainer;
 
     @BeforeEach
     void setUp() {
         try (GenericContainer<?> container = new GenericContainer<>(
-            new ImageFromDockerfile("temporary-keycloak-image", false)
+            new ImageFromDockerfile(TEMPORARY_KEYCLOAK_IMAGE, false)
                 .withDockerfile(Paths.get("src/test/resources/Dockerfile"))
                 .withTarget("builder"))) {
-            try (KeycloakContainer keycloak = new KeycloakContainer(container.getDockerImageName() + ":latest")
+            try (KeycloakContainer keycloak = new KeycloakContainer(container.getDockerImageName())
                 .useTls()
                 .withEnv("KC_HOSTNAME_STRICT", "false")
                 .withOptimizedFlag()) {
@@ -66,4 +70,15 @@ public class KeycloakContainerOptimizedTest {
         }
     }
 
+    @AfterAll
+    static void tearDown() {
+        try {
+            DockerClientFactory.lazyClient()
+                .removeImageCmd(TEMPORARY_KEYCLOAK_IMAGE)
+                .withForce(true)
+                .exec();
+        } catch (NotFoundException e) {
+            System.err.println("Failed to remove image: " + e.getMessage());
+        }
+    }
 }
