6dd6be430fb600751ba99d549016e5eb6986c122
Niko Köbler <niko@n-k.de>
2026-01-06 09:54:01 +0100

    upgrade to Keycloak 26.5
    Signed-off-by: Niko Köbler <niko@n-k.de>

--- src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
+++ src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java
@@ -67,7 +67,7 @@ public abstract class ExtendableKeycloakContainer<SELF extends ExtendableKeycloa
     public static final String ADMIN_CLI_CLIENT = "admin-cli";
 
     private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak";
-    private static final String KEYCLOAK_VERSION = "nightly";
+    private static final String KEYCLOAK_VERSION = "26.5";
 
     private static final int KEYCLOAK_PORT_HTTP = 8080;
     private static final int KEYCLOAK_PORT_HTTPS = 8443;
